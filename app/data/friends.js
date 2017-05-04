afb122b5fd2c5c238f05645dbdd83f653272c51b
Kimberly Harrod <kim@fios.com>
2017-05-04 12:53:38 -0500

    Seed friends.js

---
+++ app/data/friends.js
@@ -0,0 +1,19 @@
+[
+    {
+        "name": "Kristen",
+        "photo": "http://www.hawtcelebs.com/wp-content/uploads/2015/04/kristen-bell-we-are-the-rhoads-and-natural-health-magazine-photoshoot_2.jpg",
+        "scores": [
+            5,
+            1,
+            4,
+            4,
+            5,
+            1,
+            2,
+            5,
+            4,
+            1
+        ]
+    }
+
+]
